93cfa8c8dfec074649909d050b41daf2d2a4d4c6
Simone Cuomo <scuomo1988@gmail.com>
2019-04-26 23:48:30 +0100

    ADD "DRAFT" CONFIG FILE

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,10 @@
 var appsettings = {
-    consumerkey: '<yourconsumerkey>',
-    consumersecret: '<yourconsumersecret>',
-    bearertoken: ''
+    twitterApi: {
+        CONSUMER_KEY: "<INSERT VALUE HERE>",
+        CONSUMER_SECRET: "<INSERT VALUE HERE>",
+        ACCESS_TOKEN_KEY: "<INSERT VALUE HERE>",
+        ACCESS_TOKEN_SECRED: "<INSERT VALUE HERE>"
+    }
 };
 
 module.exports = appsettings;
