9ebf1912de0391bcf769a55bf2dffd7db3caf3c4
Simone Cuomo <scuomo1988@gmail.com>
2019-04-30 21:09:14 +0100

    push initial commits for the frontend

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TopBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
